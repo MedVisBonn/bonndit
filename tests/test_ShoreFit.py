bafecaef2794f8d5a10bf4535f4ba6f5ed49b005
Olivier Morelle <oli4morelle@gmail.com>
2018-05-24 12:36:23 +0200

    minor improvements in the tests

--- tests/test_ShoreFit.py
+++ tests/test_ShoreFit.py
@@ -14,6 +14,7 @@ from .constants import SHORE_FIT_PRECOMPUTED, DWMRI_DUMMY_DATA, \
     ODF_RESULT_HPSD, ODF_RESULT_NONNEG, ODF_RESULT_NO_CONSTRAINT
 
 data = nib.load(DWMRI_DUMMY_DATA)
+ALLOWED_ERROR = 1e-10
 
 def test_ShoreFit_deconvolution_hpsd():
     """ Here we test the deconvolution with the hpsd constraint.
@@ -23,10 +24,10 @@ def test_ShoreFit_deconvolution_hpsd():
     out, wmout, gmout, csfout = fit.fodf(data, pos='hpsd')
     tensors, mask, meta = fields.load_tensor(ODF_RESULT_HPSD)
 
-    assert ((out - tensors) < 1e-10).all() \
-    and ((wmout - fields.load_scalar(os.path.join(DRD, 'wmhpsd.nrrd'))[0]) < 1e-10).all() \
-    and ((gmout - fields.load_scalar(os.path.join(DRD, 'gmhpsd.nrrd'))[0]) < 1e-10).all() \
-    and ((csfout - fields.load_scalar(os.path.join(DRD, 'csfhpsd.nrrd'))[0]) < 1e-10).all() \
+    assert ((out - tensors) < ALLOWED_ERROR).all() \
+           and ((wmout - fields.load_scalar(os.path.join(DRD, 'wmhpsd.nrrd'))[0]) < ALLOWED_ERROR).all() \
+           and ((gmout - fields.load_scalar(os.path.join(DRD, 'gmhpsd.nrrd'))[0]) < ALLOWED_ERROR).all() \
+           and ((csfout - fields.load_scalar(os.path.join(DRD, 'csfhpsd.nrrd'))[0]) < ALLOWED_ERROR).all()
 
 
 def test_ShoreFit_deconvolution_nonneg():
@@ -37,10 +38,10 @@ def test_ShoreFit_deconvolution_nonneg():
     out, wmout, gmout, csfout = fit.fodf(data, pos='nonneg')
     tensors, mask, meta = fields.load_tensor(ODF_RESULT_NONNEG)
 
-    assert ((out - tensors) < 1e-10).all() \
-    and ((wmout - fields.load_scalar(os.path.join(DRD, 'wmnonneg.nrrd'))[0]) < 1e-10).all() \
-    and ((gmout - fields.load_scalar(os.path.join(DRD, 'gmnonneg.nrrd'))[0]) < 1e-10).all() \
-    and ((csfout - fields.load_scalar(os.path.join(DRD, 'csfnonneg.nrrd'))[0]) < 1e-10).all()
+    assert ((out - tensors) < ALLOWED_ERROR).all() \
+           and ((wmout - fields.load_scalar(os.path.join(DRD, 'wmnonneg.nrrd'))[0]) < ALLOWED_ERROR).all() \
+           and ((gmout - fields.load_scalar(os.path.join(DRD, 'gmnonneg.nrrd'))[0]) < ALLOWED_ERROR).all() \
+           and ((csfout - fields.load_scalar(os.path.join(DRD, 'csfnonneg.nrrd'))[0]) < ALLOWED_ERROR).all()
 
 
 def test_ShoreFit_deconvolution_no_constraint():
@@ -51,9 +52,7 @@ def test_ShoreFit_deconvolution_no_constraint():
     out, wmout, gmout, csfout = fit.fodf(data, pos='none')
     tensors, mask, meta = fields.load_tensor(ODF_RESULT_NO_CONSTRAINT)
 
-    assert ((out - tensors) < 1e-10).all() \
-    and ((wmout - fields.load_scalar(os.path.join(DRD, 'wmnone.nrrd'))[0]) < 1e-10).all() \
-    and ((gmout - fields.load_scalar(os.path.join(DRD, 'gmnone.nrrd'))[0]) < 1e-10).all() \
-    and ((csfout - fields.load_scalar(os.path.join(DRD, 'csfnone.nrrd'))[0]) < 1e-10).all()
-
-
+    assert ((out - tensors) < ALLOWED_ERROR).all() \
+           and ((wmout - fields.load_scalar(os.path.join(DRD, 'wmnone.nrrd'))[0]) < ALLOWED_ERROR).all() \
+           and ((gmout - fields.load_scalar(os.path.join(DRD, 'gmnone.nrrd'))[0]) < ALLOWED_ERROR).all() \
+           and ((csfout - fields.load_scalar(os.path.join(DRD, 'csfnone.nrrd'))[0]) < ALLOWED_ERROR).all()
--- tests/test_ShoreModel.py
+++ tests/test_ShoreModel.py
@@ -49,14 +49,13 @@ fit = model.fit(data, dti_vecs, wm_mask, gm_mask, csf_mask)
 
 reference_fit = mtShoreFit.load(SHORE_FIT_TEST)
 
-
-
+ALLOWED_ERROR = 1e-9
 def test_ShoreModel_signal_csf():
     """ Here we test the calculation of the response functions
     The result calculated with bonndit are compared to results from the old code
     """
 
-    assert ((reference_fit.signal_csf - fit.signal_csf) < 1e-9).all()
+    assert ((reference_fit.signal_csf - fit.signal_csf) / reference_fit.signal_csf < ALLOWED_ERROR).all()
 
 
 def test_ShoreModel_signal_gm():
@@ -64,11 +63,13 @@ def test_ShoreModel_signal_gm():
     The result calculated with bonndit are compared to results from the old code
     """
 
-    assert ((reference_fit.signal_gm - fit.signal_gm) < 1e-9).all()
+    assert ((reference_fit.signal_gm - fit.signal_gm) / reference_fit.signal_gm < ALLOWED_ERROR).all()
 
 def test_ShoreModel_signal_wm():
     """ Here we test the calculation of the response functions
     The result calculated with bonndit are compared to results from the old code
     """
 
-    assert ((reference_fit.signal_wm - fit.signal_wm) < 1e-9).all()
+    model = mtShoreModel(gtab)
+    fit = model.fit(data, dti_vecs, wm_mask, gm_mask, csf_mask)
+    assert ((reference_fit.signal_wm - fit.signal_wm) / reference_fit.signal_wm < ALLOWED_ERROR).all()
