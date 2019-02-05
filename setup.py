06d92f30a3222b8733773fa4975ca37a4eb12b3a
Olivier Morelle <oli4morelle@gmail.com>
2019-02-05 12:27:55 +0100

    Changed supported python versions and changed repository URL to MedVisBonn/bonndit

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/Oli4/bonndit',
+    url='https://github.com/MedVisBonn/bonndit',
     version='0.1.0',
     zip_safe=False,
 )
