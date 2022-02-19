c3d8749cae29ebe59505e62f8d8794ac07c16ee7
Johannes Grün <gruen.j@icloud.com>
2022-02-19 13:24:11 +0100

    update

--- src/bonndit/tracking/stopping.pyx
+++ src/bonndit/tracking/stopping.pyx
@@ -129,7 +129,7 @@ cdef class ROIInValidator(ROIInNotValidator):
 			points = open(cube)
 			points = np.array([list(map(float, point.split())) for point in points])
 			points = np.hstack((points, np.ones((points.shape[0],1)))).T
-			points = (np.array(trafo) @ np.linalg.inv(trafo_fsl) @ np.linalg.inv(trafo)@ points).T
+			points = (np.array(trafo) @ trafo_fsl @ np.linalg.inv(trafo)@ points).T
 			points = points[:,:3]
 			points = np.vstack((np.min(points, axis=0), np.max(points, axis=0)))
 			output[2*i:2*(i+1)] = points
