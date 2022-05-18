9c7ff772e2f07960e5e98fd455abf69a86e3cc34
Johannes <gruen.j@icloud.com>
2022-05-18 12:18:59 +0200

    update

--- src/bonndit/tracking/alignedDirection.c
+++ src/bonndit/tracking/alignedDirection.c
@@ -11,7 +11,7 @@
         ],
         "depends": [],
         "include_dirs": [
-            "/tmp/pip-build-env-ncw4_hq7/overlay/lib/python3.8/site-packages/numpy/core/include",
+            "/tmp/pip-build-env-hb1rw4kj/overlay/lib/python3.8/site-packages/numpy/core/include",
             "."
         ],
         "name": "bonndit.tracking.alignedDirection",
