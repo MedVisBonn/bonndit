fd4a93a8a9802b338c94af207739eb2e978bf931
Johannes <gruen@cs.uni-bonn.de>
2022-07-02 20:16:02 +0200

    test

--- src/bonndit/tracking/interpolation.pyx
+++ src/bonndit/tracking/interpolation.pyx
@@ -172,6 +172,7 @@ cdef class TrilinearFODF(Interpolation):
 		self.dist = np.zeros((3,), dtype=DTYPE)
 		self.r = kwargs['r']
 		self.rank = kwargs['rank']
+		print(sorted([[i,j,k] for i in range(-kwargs['r'],1+kwargs['r']) for j in range(-kwargs['r'],1+kwargs['r']) for k in range(-kwargs['r'],1+kwargs['r'])], key=lambda x: np.linalg.norm(kwargs['trafo']@x)))
 		self.neighbors = np.array(sorted([[i,j,k] for i in range(-kwargs['r'],1+kwargs['r']) for j in range(-kwargs['r'],1+kwargs['r']) for k in range(-kwargs['r'],1+kwargs['r'])], key=lambda x: np.linalg.norm(kwargs['trafo']@x)), dtype=np.int32)
 
 
