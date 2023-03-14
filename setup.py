1e9b995992c79dcbb05f39e8c528b13497bb57a7
Johannes <gruen@cs.uni-bonn.de>
2023-03-14 12:43:41 +0100

    update reqs

--- setup.py
+++ setup.py
@@ -157,7 +157,7 @@ ext_modules = [
     ),
 ]
 
-requirements = ['nibabel', 'numpy', 'pandas', 'dipy', 'scipy', 'tqdm',
+requirements = ['nibabel', 'numpy', 'pandas', 'fury' , 'dipy', 'fury', 'scipy', 'tqdm',
                 'cvxopt', 'mpmath', 'plyfile', 'Cython', 'pynrrd']
 
 setup_requirements = ['pytest-runner', 'cython']
