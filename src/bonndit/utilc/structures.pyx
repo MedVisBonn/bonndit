12dc74d3b6e275b7bbef28deed842e9222fc1656
Johannes <gruen@cs.uni-bonn.de>
2023-03-23 10:25:37 +0100

    parameter fix

--- src/bonndit/utilc/structures.pyx
+++ src/bonndit/utilc/structures.pyx
@@ -1,3 +1,3 @@
-cdef TijkRefineRank1 TijkRefineRank1Parm = [1e-10,1e-8,0.3,0.9,0.5,100]
-cdef TijkRefineRank TijkRefineRankkParm = [1e-10, 1e-8, 1, [1e-10,1e-8,0.3,0.9,0.5,100]]
+cdef TijkRefineRank1 TijkRefineRank1Parm = [1e-10,1e-6,0.3,0.9,0.5,50]
+cdef TijkRefineRank TijkRefineRankkParm = [1e-10, 1e-6, 1, [1e-10,1e-6,0.3,0.9,0.5,50]]
 cdef int DIM = 3
