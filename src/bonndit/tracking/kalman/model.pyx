cd62542dfc9f8d3fdc9244d8e9dae3489835e2fd
Johannes <gruen@cs.uni-bonn.de>
2023-04-06 22:38:12 +0200

    u

--- src/bonndit/tracking/kalman/model.pyx
+++ src/bonndit/tracking/kalman/model.pyx
@@ -268,11 +268,11 @@ cdef class BinghamModel(WatsonModel):
 		cdef double[:] dir = np.zeros((3,))
 		ddiagonal(&P[0,0], Pv, P.shape[0], P.shape[1])
 		for i in range(self.vector_field.shape[1]):
-			self.vector_field[2,i, <int> point[0], <int> point[1], <int> point[2]] *= -1
-			self.vector_field[4,i, <int> point[0], <int> point[1], <int> point[2]] *= -1
+			#self.vector_field[2,i, <int> point[0], <int> point[1], <int> point[2]] *= -1
+			#self.vector_field[4,i, <int> point[0], <int> point[1], <int> point[2]] *= -1
 			cart2sphere(dir, self.vector_field[2:, i, <int> point[0], <int> point[1], <int> point[2]])
-			self.vector_field[2,i, <int> point[0], <int> point[1], <int> point[2]] *= -1
-			self.vector_field[4,i, <int> point[0], <int> point[1], <int> point[2]] *= -1
+			#self.vector_field[2,i, <int> point[0], <int> point[1], <int> point[2]] *= -1
+			#self.vector_field[4,i, <int> point[0], <int> point[1], <int> point[2]] *= -1
 			mean[i*6 + 0] = self.vector_field[1,i, <int> point[0], <int> point[1], <int> point[2]]
 			# set circle by setting kappa and  beta  = 0
 			mean[i*6 + 1] = log(self.vector_field[0,i, <int> point[0], <int> point[1], <int> point[2]])
